85027e30b1344d24bde2ce764fd2c5cb5bce03c2
Romanmanco <romanmanco@gmail.com>
2022-02-09 12:20:49 +0300

    5.2.2. Реализовать Store<T extends Base> [#157 #262111]

--- src/test/java/ru/job4j/generic/UserStoreTest.java
+++ src/test/java/ru/job4j/generic/UserStoreTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.generic;
 
 import org.junit.Test;
+import ru.job4j.generics.Role;
+import ru.job4j.generics.RoleStore;
 import ru.job4j.generics.User;
 import ru.job4j.generics.UserStore;
 
@@ -69,5 +71,66 @@ public class UserStoreTest {
         store.delete("10");
         User result = store.findById("1");
         assertThat(result.getUsername(), is("Petr"));
+    }
+
+    @Test
+    public void whenAddAndFind() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        Role result = roleStore.findById("1");
+        assertThat(result.getRolename(), is("Driver"));
+    }
+
+    @Test
+    public void whenAddAndFindIsNull() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        Role result = roleStore.findById("10");
+        assertNull(result);
+    }
+
+    @Test
+    public void whenAddDuplicateAndFindIsDriver() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        roleStore.add(new Role("1", "Baker"));
+        Role result = roleStore.findById("1");
+        assertThat(result.getRolename(), is("Driver"));
+    }
+
+    @Test
+    public void whenReplaceThenRolenameIsBaker() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        roleStore.replace("1", new Role("1", "Baker"));
+        Role result = roleStore.findById("1");
+        assertThat(result.getRolename(), is("Baker"));
+    }
+
+    @Test
+    public void whenNoReplaceUserThenNoChangeRolename() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        roleStore.replace("10", new Role("10", "Baker"));
+        Role result = roleStore.findById("1");
+        assertThat(result.getRolename(), is("Driver"));
+    }
+
+    @Test
+    public void whenDeleteUserThenRoleIsNull() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        roleStore.delete("1");
+        Role result = roleStore.findById("1");
+        assertNull(result);
+    }
+
+    @Test
+    public void whenNoDeleteUserThenRolenameIsDriver() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("1", "Driver"));
+        roleStore.delete("10");
+        Role result = roleStore.findById("1");
+        assertThat(result.getRolename(), is("Driver"));
     }
 }

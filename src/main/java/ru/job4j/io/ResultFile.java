cb12361ebed2c179094197cde64052e9461f92e6
Romanmanco <romanmanco@gmail.com>
2022-03-02 15:01:10 +0300

    Добавлена запись с помощью буфера.

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,12 +1,14 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
-            in.lines().forEach(System.out::println);
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.println("Hello, world!");
         } catch (Exception e) {
             e.printStackTrace();
         }

6d8e1d6b4837fc9554263e978b6802efde3fe801
Romanmanco <romanmanco@gmail.com>
2022-03-02 14:57:43 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader objReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = objReader.readLine()) != null) {
+                String[] strings = line.split(" ");
+                if ("404".equals(strings[strings.length - 2])) {
+                    list.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        System.out.println(log);
+    }
+}
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,17 +1,12 @@
 package ru.job4j.io;
 
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int i = 1; i <= 10; i++) {
-                for (int j = 1; j <= 10; j++) {
-                    out.write((i + " * " + j + " = " + i * j).getBytes());
-                    out.write(System.lineSeparator().getBytes());
-                }
-                out.write(System.lineSeparator().getBytes());
-            }
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            in.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }

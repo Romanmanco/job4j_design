725ec7e76c90e646df54fc75d947db62a12b67f4
Romanmanco <romanmanco@gmail.com>
2022-03-09 12:30:39 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null.");
+        }
+        Path start = Paths.get(args[0]);
+        if (!start.toFile().exists() || !start.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Root folder is not valid.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("File extension is not valid.");
+        }
+        search(start, p -> p.toFile()
+                .getName()
+                .endsWith(args[1]))
+                .forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}

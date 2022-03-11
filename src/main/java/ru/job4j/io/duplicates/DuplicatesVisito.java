754e57d5ad0f386a1629129e9f58d32721d5137a
Romanmanco <romanmanco@gmail.com>
2022-03-11 17:37:48 +0300

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisito.java
@@ -0,0 +1,40 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    private Map<FileProperty, List<Path>> duplicatesMap;
+
+    public DuplicatesVisitor() {
+        this.duplicatesMap = duplicatesMap;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (!duplicatesMap.containsKey(fileProperty)) {
+            duplicatesMap.put(fileProperty, new ArrayList<>());
+        }
+        duplicatesMap.get(fileProperty).add(file.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    public void duplicatesShow() {
+        for (Map.Entry<FileProperty, List<Path>> entry : duplicatesMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.println("file (" + entry.getKey().getName() + ") found in: ");
+                for (Path path : entry.getValue()) {
+                    System.out.println("\t" + path.toAbsolutePath());
+                }
+            }
+        }
+    }
+}

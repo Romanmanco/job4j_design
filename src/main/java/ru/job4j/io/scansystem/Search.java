6f68a6971a4b54de58c0ba8154d3432b1af5ba4e
Romanmanco <romanmanco@gmail.com>
2022-03-12 12:40:24 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/scansystem/Search.java
+++ src/main/java/ru/job4j/io/scansystem/Search.java
@@ -10,14 +10,14 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
-            throw new IllegalArgumentException("Root folder is null.");
+            throw new IllegalArgumentException("Not valid arguments. Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION.");
         }
         Path start = Paths.get(args[0]);
         if (!start.toFile().exists() || !start.toFile().isDirectory()) {
-            throw new IllegalArgumentException("Root folder is not valid.");
+            throw new IllegalArgumentException("Not valid arguments. Root folder is not valid.");
         }
         if (!args[1].startsWith(".")) {
-            throw new IllegalArgumentException("File extension is not valid.");
+            throw new IllegalArgumentException("Not valid arguments. File extension is not valid.");
         }
         search(start, p -> p.toFile()
                 .getName()
@@ -29,6 +29,5 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
-
     }
 }

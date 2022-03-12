471b462949d53b0494c9d222ef19ac76c518abe0
Romanmanco <romanmanco@gmail.com>
2022-03-12 12:21:32 +0300

    4.2. Поиск дубликатов [#315066 #274072]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,11 +3,15 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Files.walkFileTree(Path.of("C:\\Users\\User"), visitor);
-        visitor.duplicatesShow();
+        Path start = Paths.get("./");
+        Files.walkFileTree(start, visitor);
+        List<Path> duplicates = visitor.showDuplicates();
+        duplicates.forEach(System.out::println);
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisito.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisito.java
@@ -6,35 +6,32 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private Map<FileProperty, List<Path>> duplicatesMap;
-
-    public DuplicatesVisitor() {
-        this.duplicatesMap = duplicatesMap;
-    }
+    Map<FileProperty, List<Path>> files = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (!duplicatesMap.containsKey(fileProperty)) {
-            duplicatesMap.put(fileProperty, new ArrayList<>());
+        FileProperty filePropertys = new FileProperty(attrs.size(), file.getFileName().toString());
+        List<Path> list = files.get(filePropertys);
+        if (list == null) {
+            list = new ArrayList<>();
         }
-        duplicatesMap.get(fileProperty).add(file.toAbsolutePath());
-        return FileVisitResult.CONTINUE;
+        list.add(file);
+        files.put(filePropertys, list);
+        return super.visitFile(file, attrs);
     }
 
-    public void duplicatesShow() {
-        for (Map.Entry<FileProperty, List<Path>> entry : duplicatesMap.entrySet()) {
-            if (entry.getValue().size() > 1) {
-                System.out.println("file (" + entry.getKey().getName() + ") found in: ");
-                for (Path path : entry.getValue()) {
-                    System.out.println("\t" + path.toAbsolutePath());
-                }
-            }
-        }
+    public List<Path> showDuplicates() {
+        List<Path> duplicate = new ArrayList<>();
+        files.values()
+                .stream()
+                .filter(el -> el.size() > 1)
+                .forEach(duplicate::addAll);
+        return duplicate;
     }
 }

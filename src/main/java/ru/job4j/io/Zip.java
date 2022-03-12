c8d22adfb57a64be3e2e80d594baf0cba6acbe4e
Romanmanco <romanmanco@gmail.com>
2022-03-12 22:42:24 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,71 @@
+package ru.job4j.io;
+
+import ru.job4j.io.scansystem.Search;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    public static void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(path.toFile().getPath()))) {
+                    zip.write(in.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+
+        checkValidNumber(args);
+        ArgsName.of(args);
+
+        ArgsName arguments = ArgsName.of(args);
+        String sourceDir = arguments.get("d");
+        Path sourceFile = Paths.get(sourceDir);
+
+        checkSource(sourceDir, sourceFile);
+
+        String excludedFile = arguments.get("e").startsWith(".") ? arguments.get("e") : "." + arguments.get("e");
+        String dir = arguments.get("o");
+        File file = new File(dir);
+
+        List<Path> search = Search.search(sourceFile, el -> !el.toFile().getName().endsWith(excludedFile));
+
+        packFiles(search, file);
+    }
+
+    private static void checkSource(String directory, Path file) {
+        if (directory.isEmpty() || !file.toFile().exists()) {
+            throw new IllegalArgumentException("wrong argument: " + directory + ". Path " + directory + " doesn't exist.");
+        }
+    }
+
+    private static void checkValidNumber(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Invalid number.");
+        }
+    }
+}

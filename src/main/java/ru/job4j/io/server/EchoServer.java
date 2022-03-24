64e2cfc36abe2e9f63035e019e1d62b7d59a1061
Romanmanco <romanmanco@gmail.com>
2022-03-24 14:27:34 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/server/EchoServer.java
+++ src/main/java/ru/job4j/io/server/EchoServer.java
@@ -12,13 +12,24 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String[] line;
+                    String answer = "";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.indexOf("Bye") != 0) {
-                            server.close();
+                        if (str.contains("?msg=")) {
+                            line = str.split(" ");
+                            if ("Exit".equals(line[1].substring(6))) {
+                                answer = "Bye!";
+                                server.close();
+                            } else if ("Hello".equals(line[1].substring(6))) {
+                                answer = "Hello, dear friend.";
+                            } else {
+                                answer = "What.";
+                            }
                         }
                         System.out.println(str);
                     }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                     out.flush();
                 }
             }

3f96e31c6145d9ddb5749f473311bc9e4c49f1b9
Romanmanco <romanmanco@gmail.com>
2022-03-29 10:49:53 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -5,14 +5,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class UsageLog4j {
 
+    /**
+     *         Вывод сообщений в логе.
+     *         LOG.trace("trace message");
+     *         LOG.debug("debug message");
+     *         LOG.info("info message");
+     *         LOG.warn("warn message");
+     *         LOG.error("error message");
+     */
+
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Roman Manco";
+        int age = 25;
+        boolean male = true;
+        char charValue = 1234;
+        short shortValue = 12345;
+        long longValue = 144999;
+        float floatValue = 235.6f;
+        double doubleValue = 65.4;
+        LOG.debug("User info name : {}, age : {}, male : {}, charValue : {}, shortValue : {}, longValue : {}, floatValue : {}, doubleValue : {}",
+                name, age, male, charValue, shortValue, longValue, floatValue, doubleValue);
     }
 }

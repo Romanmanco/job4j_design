fc0a7601aa6737b25a0795b95e31d3b3a083b7de
Romanmanco <romanmanco@gmail.com>
2022-04-01 12:39:24 +0300

    2. Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/io/search/FinderFiles.java
@@ -0,0 +1,32 @@
+package ru.job4j.io.search;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FinderFiles extends SimpleFileVisitor<Path> {
+
+    private final Predicate<String> condition;
+    private final List<Path> pathList;
+
+    public FinderFiles(Predicate<String> condition) {
+        this.condition = condition;
+        this.pathList = new ArrayList<>();
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (condition.test(file.toString())) {
+            pathList.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return pathList;
+    }
+}

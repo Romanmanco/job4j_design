d905950321ec26be0cbfcd7d0a952995ae4ecdbc
Romanmanco <romanmanco@gmail.com>
2022-04-02 12:31:56 +0300

    0. Что такое база данных. [#6860 #283267]

--- src/main/java/ru/job4j/sql/auto.sql
+++ src/main/java/ru/job4j/sql/auto.sql
@@ -1,13 +1,17 @@
-CREATE TABLE auto (
-    id serial primary key,
-    name text,
-    speed varchar,
-    price int
-);
-insert into auto(name, speed, price) values ('ford','mini','ferrari');
-update auto set speed = '150 m/h';
-update auto set price = '200000$';
-update auto set price = '150000 $' where id = 2;
-delete from auto where price = '150000 $';
-
+create table auto (
+id serial primary key,
+name text,
+speed varchar,
+price int
+)
+insert into auto(name, speed, price) values ('ford', 150, 125000);
+select * from auto;
+insert into auto(name, speed, price) values ('audi', 175, 165000);
+select * from auto;
+insert into auto(name, speed, price) values ('volvo', 135, 111500);
+select * from auto;
+select speed from auto;
+update auto set price = 150000 where name = 'volvo';
+select * from auto;
+delete from auto where price = 150000;
 select * from auto;

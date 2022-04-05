228ca8c51bc5cc13ca2d71bcafb8266fc1ac2670
Romanmanco <romanmanco@gmail.com>
2022-04-05 14:06:48 +0300

    1. Inner join [#373617]

---
+++ src/main/java/ru/job4j/sql/aliance.sql
@@ -0,0 +1,31 @@
+create table ticket_cinema (
+id serial primary key,
+name_film text,
+cost int
+);
+
+create table visitors_cinema (
+id serial primary key,
+name varchar(255),
+ticket_id int references ticket(id) unique
+);
+
+insert into ticket_cinema(name_film, cost) values ('Операция Ы', 1450);
+insert into ticket_cinema(name_film, cost) values ('Бросок кобры', 1200);
+insert into ticket_cinema(name_film, cost) values ('Грозовые ворота', 1150);
+
+insert into visitors_cinema(name, ticket_id) values ('Ivan', 1);
+insert into visitors_cinema(name, ticket_id) values ('Boris', 2);
+insert into visitors_cinema(name, ticket_id) values ('Petr', 3);
+insert into visitors_cinema(name) values ('Vasya');
+insert into visitors_cinema(name) values ('Anya');
+
+select pp.name, p.name_film, p.cost
+from visitors_cinema as pp
+join ticket_cinema as p
+on pp.ticket_id = p.id;
+
+select pp.name as Имя, p.name_film as Название,
+p.cost as Номер from visitors_cinema as pp
+join ticket_cinema as p
+on pp.ticket_id = p.id;

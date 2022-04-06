a0d2086a58bf1a1f932179b747870c123b95f3eb
Romanmanco <romanmanco@gmail.com>
2022-04-06 18:55:38 +0300

    0. Join [#6862 #285276]

--- src/main/java/ru/job4j/sql/join/join_departments.sql
+++ src/main/java/ru/job4j/sql/join/join_departments.sql
@@ -9,6 +9,12 @@ create table departments (
     emploers_id int references emploers(id)
 );
 
+create table teens (
+id serial primary key,
+"name" text,
+gender text
+);
+
 insert into emploers(name_emploer) values ('Работник 1');
 insert into emploers(name_emploer) values ('Работник 2');
 insert into emploers(name_emploer) values ('Работник 3');
@@ -20,6 +26,10 @@ insert into departments(name_department, emploers_id) values ('Оодел 4', 1)
 insert into departments(name_department, emploers_id) values ('Оодел 5', null);
 insert into departments(name_department, emploers_id) values ('Оодел 6', null );
 
+insert into teens(name, gender) values ('Имя1', 'Гендер1');
+insert into teens(name, gender) values ('Имя2', 'Гендер2');
+insert into teens(name, gender) values ('Имя3', 'Гендер3');
+
 select * from emploers e
 left join departments d on e.id = d.emploers_id;
 
@@ -27,8 +37,22 @@ select * from departments d
 right join emploers e on d.emploers_id = e.id;
 
 select * from departments d
+left join emploers e
+on d.emploers_id = e.id where e.id is null;
+
+select * from departments d
 full join emploers e on d.emploers_id = e.id;
 
 select e1.name_emploer as Работник, e2.name_emploer as Работник
 from emploers e1
 cross join emploers e2;
+
+select * from departments d
+left join emploers e on e.id = d.emploers_id;
+
+select * from departments d
+right join emploers e on e.id = d.emploers_id;
+
+select e1.name as Имя, e2.gender as Гендер
+from teens e1
+cross join teens e2;

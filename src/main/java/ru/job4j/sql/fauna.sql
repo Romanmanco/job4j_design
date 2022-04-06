3d61580138d5bdb806febaf50bc2d07a1f0d4454
Romanmanco <romanmanco@gmail.com>
2022-04-06 12:51:39 +0300

    0. Select [#6861 #284731]

--- src/main/java/ru/job4j/sql/fauna.sql
+++ src/main/java/ru/job4j/sql/fauna.sql
@@ -6,19 +6,19 @@ create table fauna (
 );
 
 insert into fauna(name, avg_age, discovery_date)
-values ('Tiger', 50000, '2018-09-02')
+values ('Tiger', 50000, '2018-09-02');
 insert into fauna(name, avg_age, discovery_date)
-values ('Beer', 17777, '2022-01-11')
+values ('Beer', 17777, '2022-01-11');
 insert into fauna(name, avg_age, discovery_date)
-values ('Bee', 12000, '2021-12-07')
+values ('Bee', 12000, '2021-12-07');
 insert into fauna(name, avg_age, discovery_date)
-values ('Fish', 20000, '2022-03-09')
+values ('Fish', 20000, '2022-03-09');
 insert into fauna(name, avg_age, discovery_date)
-values ('Dog', 20000, null )
+values ('Dog', 20000, null );
 insert into fauna(name, avg_age, discovery_date)
-values ('Someone', null, '1950-06-03')
+values ('Someone', null, '1950-06-03');
 insert into fauna(name, avg_age, discovery_date)
-values ('Someone', null, '1949-07-02')
+values ('Someone', null, '1949-07-02');
 
 select * from fauna;
 select * from fauna where name like 'Fish%';

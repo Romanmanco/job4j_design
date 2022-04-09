d35a0f30061fb6bea69647d4672c1c17c6689642
Romanmanco <romanmanco@gmail.com>
2022-04-09 11:37:39 +0300

    3. Фильтры [#1732 #285268]

--- src/main/java/ru/job4j/sql/product/create.sql
+++ src/main/java/ru/job4j/sql/product/create.sql
@@ -7,18 +7,19 @@ create table product (
 id serial primary key,
 name_product text,
 type_id int references type_prod(id),
-expired_date text,
+expired_date date,
 value int,
 price int
 );
 
-select * from product where type_id = 1;
+select * from type_prod where name_type = 'СЫР';
 
 select * from product where name_product like '%мороженое%';
 
-select * from product where expired_date < '2022.04.06';
+select * from product where expired_date < current_date;
 
-select max(price) from product;
+select max(price) as Максилмальная_цена
+from product;
 
 select s.name_type as Тип, ss.value as Остаток
 from product as ss
@@ -26,7 +27,12 @@ join type_prod s
 on ss.type_id = s.id
 group by s.name_type, ss.value;
 
-select * from product where type_id = 1 or type_id = 2;
+select s.name_type as Тип, ss.name_product as Наименование
+from product as ss
+join type_prod s
+on ss.type_id = s.id
+group by s.name_type, ss.name_product;
+
 
 select s.name_type as Тип, sum(value) as Остаток
 from product as ss
@@ -34,5 +40,3 @@ join type_prod s
 on ss.type_id = s.id
 group by s.name_type, ss.value
 having sum(ss.value) < 10;
-
-select * from product;

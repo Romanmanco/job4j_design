6d89c9a891ae871fb91b421f12ba5b803260cb33
Romanmanco <romanmanco@gmail.com>
2022-04-13 11:33:09 +0300

    2. Группировка и агрегатные функции [#373618 #285226]

--- src/main/java/ru/job4j/sql/devices/create.sql
+++ src/main/java/ru/job4j/sql/devices/create.sql
@@ -15,34 +15,25 @@ create table devices_people (
     device_id int references devices(id)
 );
 
-create table price_devices_people (
-    id serial primary key,
-    name_customer int references people(id),
-    device_id int references devices(id),
-    people_id int references people(id)
-);
-
---drop table devices;
---drop table people;
---drop table devices_people;
+drop table devices_people;
+drop table people;
+drop table devices;
 
 select avg(price) from devices;
 
-select p.name_customer as Пользователь, avg(dp.device_id) as Средняя_цена_его_устройств
-from people as p
-join devices_people dp
-on p.id = dp.device_id
+select p.name_customer as Пользователь, avg(d.price) as Средняя_цена_его_устройств
+from devices_people as dp
+join people p
+on dp.people_id = p.id
+join devices as d
+on dp.device_id = d.id
 group by p.name_customer;
 
---select p.name_customer as Пользователь, avg(d.price) as Средняя_цена
---from people as p
---join devices d
---on p.id = d.id
---group by p.name_customer;
-
-select s.name_device, avg(s.price)
-from devices_people as ss
-join devices s
-on ss.device_id = s.id
-group by s.name_device
-having avg(s.price) > 5000;
+select p.name_customer as Пользователь, avg(d.price) as Средняя_цена_его_устройств
+from devices_people as dp
+join people p
+on dp.people_id = p.id
+join devices as d
+on dp.device_id = d.id
+group by p.name_customer
+having avg(d.price) > 5000;
--- src/main/java/ru/job4j/sql/devices/insert.sql
+++ src/main/java/ru/job4j/sql/devices/insert.sql
@@ -1,12 +1,11 @@
 insert into devices(name_device, price)
-values ('apple', 150), ('samsung', 125), ('huawei', 105), ('nokia', 5001);
+values ('apple', 6000), ('samsung', 4000), ('huawei', 3000), ('nokia', 2000);
 select * from devices;
 
 insert into people(name_customer) values ('Вася'), ('Петя'), ('Игорь');
 select * from people;
 
 insert into devices_people(people_id, device_id) values (1, 1), (1, 2), (1, 3);
-insert into devices_people(people_id, device_id) values (2, 2), (2, 2), (2, 2);
-insert into devices_people(people_id, device_id) values (3, 1), (3, 3), (3, 3);
-
+insert into devices_people(people_id, device_id) values (2, 2), (2, 1), (2, 2);
+insert into devices_people(people_id, device_id) values (3, 1), (3, 1), (3, 1);
 select * from devices_people;

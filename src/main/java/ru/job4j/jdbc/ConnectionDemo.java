59c713a351e2fae8d0d6016f4cb1880319596ec6
Romanmanco <romanmanco@gmail.com>
2022-04-18 10:22:47 +0300

    0. JDBC [#6863 #288913]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -8,17 +8,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
-
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Config config = new Config("app.properties");
+    public static void main(String[] args) {
+        Config config = new Config("C:/projects/job4j_design/src/main/java/resources/app.properties");
         config.load();
         try {
             Class.forName(config.value("driver"));
             System.out.println("Driver connected");
             String url = config.value("url");
-            String login = config.value("username");
+            String username = config.value("username");
             String password = config.value("password");
-            try (Connection connect = DriverManager.getConnection(url, login, password)) {
+            try (Connection connect = DriverManager.getConnection(url, username, password)) {
                 System.out.println("Database connection established");
                 DatabaseMetaData metaData = connect.getMetaData();
                 System.out.println(metaData.getUserName());
